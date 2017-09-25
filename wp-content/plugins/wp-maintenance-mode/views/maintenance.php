01fd1f80d410c53d742fcfdb255705f665c56e74
Matteo Giaccone <matteo.giaccone@vitsoe.com>
2017-09-25 23:19:09 +0200

    Add maintenance mode and maintenance page

---
+++ wp-content/plugins/wp-maintenance-mode/views/maintenance.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Piemonte Fabbriche Aperte</title>
+	<link href="/wp-content/plugins/wp-maintenance-mode/assets/css/main.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+	<div class="full-hd">
+		<img src="/wp-content/plugins/wp-maintenance-mode/assets/images/full-hd.jpg" />
+	</div>
+
+	<div class="mobile">
+		<img src="/wp-content/plugins/wp-maintenance-mode/assets/images/mobile.jpg" />
+	</div>
+
+
+</body>
+</html>
