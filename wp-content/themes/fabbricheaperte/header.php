632659f509f09e16d3ad598e868f67ab96f4be78
Matteo Giaccone <matteo.giaccone@vitsoe.com>
2017-10-02 16:35:22 +0200

    Revert "Change analytics"
    This reverts commit e6bb8c8986445026d1d720b927b235126115a3a6.

--- wp-content/themes/fabbricheaperte/header.php
+++ wp-content/themes/fabbricheaperte/header.php
@@ -24,14 +24,15 @@
 <link rel="icon" href="<?= get_stylesheet_directory_uri() ?>/icon/favicon.ico" type="image/x-icon">
 
 <?php wp_head(); ?>
-<!-- Global Site Tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107401740-1"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)};
-  gtag('js', new Date());
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  gtag('config', 'UA-107401740-1');
+  ga('create', 'UA-97376718-1', 'auto');
+  ga('send', 'pageview');
+
 </script>
 </head>
 
