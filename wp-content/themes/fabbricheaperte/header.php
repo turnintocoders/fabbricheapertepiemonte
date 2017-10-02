7f2f027ae2c15919681b0c184ee0b772f72161d5
Matteo Giaccone <matteo.giaccone@vitsoe.com>
2017-10-02 16:35:55 +0200

    Change tracking

--- wp-content/themes/fabbricheaperte/header.php
+++ wp-content/themes/fabbricheaperte/header.php
@@ -30,7 +30,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-97376718-1', 'auto');
+  ga('create', 'UA-107401740-1', 'auto');
   ga('send', 'pageview');
 
 </script>
