9979e2293e3f5858c5a4864795b5d2c3cf28393b
Matteo Giaccone <matteo.giaccone@vitsoe.com>
2017-10-03 21:25:08 +0200

    Refactor fabbriche

--- wp-content/themes/fabbricheaperte/archive-fabbriche.php
+++ wp-content/themes/fabbricheaperte/archive-fabbriche.php
@@ -40,75 +40,73 @@ query_posts( $query );
 
 ?>
 
-<div class="wrap">
-	<div id="fabbrica-container">
-		<div id="fabbrica-header">
-			<form method="get" class="search-form" action="/fabbriche">
-				<div class="gallery gallery-columns-3 gallery-size-thumbnail">
-					<div class="gallery-item">
-						<label for="quartiere">Provincia: </label>
-						<select id="quartiere" name="provincia">
-							<option value="">Vedi tutti</option>
-							<?php $province = get_field_object('field_59c5168825d53')['choices'];
-							foreach ($province as $key => $provincia) {
-								$selected = $provincia == $selected_provincia ? ' selected' : '';
-								echo '<option value="'.$key.'"'.$selected.'>'.$provincia.'</option>';
-							} ?>
-						</select>
-					</div><div class="gallery-item">
-						<label for="tipologia">Categoria: </label>
-						<select id="tipologia" name="category">
-							<option value="">Vedi tutti</option>
-							<?php $categories = get_field_object('field_59c517c86992c')['choices'];
-							foreach ($categories as $key => $category) {
-								$selected = $key == $selected_category ? ' selected' : '';
-								echo '<option value="'.$key.'"'.$selected.'>'.$category.'</option>';
-							} ?>
-						</select>
-					</div><div class="gallery-item">
-						<label>&nbsp;</label>
-						<button type="submit" class="form-submit">
-							Filtra
-						</button>
-					</div>
-				</div>
-			</form>
-
-			<?php
-			if ( array_key_exists( 'provincia', $_GET ) && !empty( $_GET['provincia'] ) ) {
-				echo '<b>Provincia: </b>'.$selected_provincia.' ';
-			}
-			if ( array_key_exists( 'category', $_GET ) && !empty( $_GET['category'] ) ) {
-				echo '<b>Categoria: </b>'.get_field_object('field_59c517c86992c')['choices'][$selected_category].' ';
-			}
-			if ( ( array_key_exists( 'provincia', $_GET ) && !empty( $_GET['provincia'] ) ) ||
-				 ( array_key_exists( 'category', $_GET ) && !empty( $_GET['category'] ) ) ) {
-				echo '<a href="/fabbriche">Vedi tutti</a>';
-			}
-			?>
+<div class="wrap margin-bottom">
+	<form method="get" class="search-form" action="/fabbriche">
+		<div class="gallery gallery-columns-3 gallery-size-thumbnail">
+			<div class="gallery-item">
+				<label for="quartiere">Provincia: </label>
+				<select id="quartiere" name="provincia">
+					<option value="">Vedi tutti</option>
+					<?php $province = get_field_object('field_59c5168825d53')['choices'];
+					foreach ($province as $key => $provincia) {
+						$selected = $provincia == $selected_provincia ? ' selected' : '';
+						echo '<option value="'.$key.'"'.$selected.'>'.$provincia.'</option>';
+					} ?>
+				</select>
+			</div><div class="gallery-item">
+				<label for="tipologia">Categoria: </label>
+				<select id="tipologia" name="category">
+					<option value="">Vedi tutti</option>
+					<?php $categories = get_field_object('field_59c517c86992c')['choices'];
+					foreach ($categories as $key => $category) {
+						$selected = $key == $selected_category ? ' selected' : '';
+						echo '<option value="'.$key.'"'.$selected.'>'.$category.'</option>';
+					} ?>
+				</select>
+			</div><div class="gallery-item">
+				<label>&nbsp;</label>
+				<button type="submit" class="form-submit">
+					Filtra
+				</button>
+			</div>
 		</div>
+	</form>
 
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
-				<div class="item-fabbrica">
-				<?php
-				/* Start the Loop */
-				if ( have_posts() ) :
-					while ( have_posts() ) : the_post();
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'template-parts/fabbriche-thumb' );
-					endwhile;
-				else :
-					get_template_part( 'template-parts/post/content', 'none' );
-				endif; ?>
-				</div>
-			</main><!-- #main -->
-		</div><!-- #primary -->
-	</div>
-</div><!-- .wrap -->
+	<?php
+	if ( array_key_exists( 'provincia', $_GET ) && !empty( $_GET['provincia'] ) ) {
+		echo '<b>Provincia: </b>'.$selected_provincia.' ';
+	}
+	if ( array_key_exists( 'category', $_GET ) && !empty( $_GET['category'] ) ) {
+		echo '<b>Categoria: </b>'.get_field_object('field_59c517c86992c')['choices'][$selected_category].' ';
+	}
+	if ( ( array_key_exists( 'provincia', $_GET ) && !empty( $_GET['provincia'] ) ) ||
+		 ( array_key_exists( 'category', $_GET ) && !empty( $_GET['category'] ) ) ) {
+		echo '<a href="/fabbriche">Vedi tutti</a>';
+	}
+	?>
+</div>
+
+<div class="fabbriche-background">
+	<div class="wrap">
+		<main id="main" class="site-main" role="main">
+			<div class="item-fabbrica">
+			<?php
+			/* Start the Loop */
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post();
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/fabbriche-thumb' );
+				endwhile;
+			else :
+				get_template_part( 'template-parts/post/content', 'none' );
+			endif; ?>
+			</div>
+		</main><!-- #main -->
+	</div><!-- .wrap -->
+</div>
 
 <?php get_footer();
