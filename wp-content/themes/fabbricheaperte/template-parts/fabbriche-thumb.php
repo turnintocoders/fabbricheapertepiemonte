b90bc29126a8dc1c5f05a3071b01e62e217a62e3
Matteo Giaccone <matteo.giaccone@vitsoe.com>
2017-10-11 22:41:13 +0200

    Add thumb to fabbriche list

--- wp-content/themes/fabbricheaperte/template-parts/fabbriche-thumb.php
+++ wp-content/themes/fabbricheaperte/template-parts/fabbriche-thumb.php
@@ -1,5 +1,14 @@
+<?php
+$style = '';
+$thumb_id = get_post_thumbnail_id();
+$thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true);
+if ($thumb_id) {
+	$style = 'style="background: url('.$thumb_url[0].');background-size: cover;"';
+}
+?>
+
 <a href="<?= get_permalink() ?>">
-	<div class="page-item">
+	<div class="page-item" <?= $style ?>>
 		<p>↓</p>
 		<p><?= get_the_title() ?></p>
 	</div>
